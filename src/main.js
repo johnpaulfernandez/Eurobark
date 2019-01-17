6e3063df45f5de2ca6a9f40abe655035019df93e
johnpaulfernandez <engr.jfernandez@gmail.com>
2019-01-17 15:54:25 -0800

    Replace vuetify with bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
+import BootstrapVue from "bootstrap-vue";
 
-Vue.use(Vuetify);
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
